bef7e4fb066ef84f5a50493a9d448daa307eda58
Alco2 <abdou.lahoub.wwe@gmail.com>
2023-03-22 22:58:24 +0000

    change quote dessign

--- src/assets/quotes/Quotes.jsx
+++ src/assets/quotes/Quotes.jsx
@@ -15,14 +15,15 @@ const Quotes = () => {
       return <div>Loading...</div>;
     }
   return (
-    <div className='bg-primary text-white ml-40 pl-10 min-h-screen h-max pb-10 '>
+    <div className="bg-primary bg-center bg-cover text-white ml-40 pl-10 min-h-screen h-max pb-10 ">
         <div className='flex justify-center'>
-            
+    
         </div>
-        <div className='w-full grid place-items-center'>
-          <div><h1 className='text-lg font-bold bg-cyan-800 shadow-md py-2 font-mono px-5 rounded-md mb-5'>{quote.anime}</h1></div>
-          <div className='w-1/2 font-mono bg-lime-900 p-2 rounded'><p className='text-sm'>{quote.quote}</p></div>
+        <div className='w-1/2 grid place-items-center translate-y-5'><h1 className='text-lg font-bold bg-blue-600 shadow-md py-2 font-mono px-5 rounded-md mb-5'>{quote.anime}</h1></div>
+        <div className="grid place-items-center bg-[url('https://i.pinimg.com/736x/60/cb/46/60cb4600ad2427938722b77faba6426a.jpg')] bg-cover rounded-md h-30 w-2/4 ml-60">
+          <div className='font-mono'><p className='text-md font-bold p-10'>{quote.quote}</p></div>
         </div>
+        <div className='w-1/2 grid place-items-center translate-x-52'><p className='text-lg font-bold bg-red-600 shadow-md py-2 font-mono px-5 rounded-md mb-5'>{quote.character}</p></div>
     </div>
   )
 }
